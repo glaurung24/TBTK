fb183c42a0b14c727804e47f1b506e8786fdafea
glaurung24 <andreas.theiler@gmx.at>
2023-03-10 17:30:52 +0100

    Hipified GPUResourceManager.cpp

--- Lib/src/hip/GPUResourceManager.cpp
+++ Lib/src/hip/GPUResourceManager.cpp
@@ -13,20 +13,36 @@
  * limitations under the License.
  */
 
-/** @file GPUResourceManagerDummy.cpp
- *
+/** @file GPUResourceManager.cu
+ *  
  *  @author Kristofer Björnson
  */
 
 #include "TBTK/GPUResourceManager.h"
+#include "TBTK/Streams.h"
+
+using namespace std;
 
 namespace TBTK{
 
 void GPUResourceManager::createDeviceTable(){
-	numDevices = 0;
+	hipGetDeviceCount(&numDevices);
+
+	if(getGlobalVerbose())
+		Streams::out << "Num GPU devices: " << numDevices << "\n";
+
+	if(numDevices > 0){
+		busyDevices = new bool[numDevices];
+		for(int n = 0; n < numDevices; n++)
+			busyDevices[n] = false;
+	}
 }
 
 void GPUResourceManager::destroyDeviceTable(){
+	if(numDevices > 0){
+		delete [] busyDevices;
+		busyDevices = NULL;
+	}
 }
 
 };	//End of namespace TBTK
